bc785c6cac2682e4a904a4330c6e3969c511d69b
Jasemonu <jasemonu@gmail.com>
2023-09-30 18:41:10 +0000

    practice

--- js_promises/ice_creame_stock.js
+++ js_promises/ice_creame_stock.js
@@ -5,7 +5,7 @@ let stocks = {
   toppings: ["chocolate", "peanuts"]
 };
 
-let is_shop_ope = true;
+let is_shop_open = true;
 
 let order = (time, work) => {
   return new Promise( (resolve, reject) => {
