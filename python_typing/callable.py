5c2e9bcb42e7cb6d198926d7877431533b903e04
Jasemonu <jasemonu@gmail.com>
2023-10-08 15:47:12 +0000

    callable.py

---
+++ python_typing/callable.py
@@ -0,0 +1,13 @@
+from typing import List, Dict, Set, Optional, Any, Sequence, Tuple, Callable
+
+def foo() -> Callable[[int, int] int]) -> None:
+    def add(x: int, y: int) -> int:
+        return x + y
+
+    return add
+
+
+foo() 
+
+
+
